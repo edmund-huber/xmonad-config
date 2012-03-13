e8d700bc3aa4788c468729d3ad19f8d54eb87904
Edmund Huber <edmundhuber@google.com>
2012-03-12 21:02:28 -0700

    start of suggestions

---
+++ suggested/disable-terminal-menubar.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+gconftool --type boolean --set /apps/gnome-terminal/profiles/Default/default_show_menubar false
---
+++ suggested/disable-terminal-scrollbar.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/scrollbar_position --type string hidden
